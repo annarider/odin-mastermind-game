b1291cb3099bc82ff60e9e34d8865af74738d8b3
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-07 11:12:49 -0700

    Refactor: Test to split into smaller test methods
     I didn't want all my print statements in 1 method
     because I don't want to keep commenting and
     uncommenting print statements.
    
     To break the method into smaller methods and call
     each method when I wanted to check it in the FILE
     block, I had to change to use class variables.

--- test/test_play.rb
+++ test/test_play.rb
@@ -7,15 +7,28 @@ require_relative 'test_setup'
 require_relative '../lib/board'
 
 module TestPlay
-  def self.create_game
-    code = TestSetup.generate_code
-    board = Board.new(code)
-    p "board: #{board}"
-    p "check code guess: #{board.correct?(code)}"
-    p "check random guess: #{board.correct?(code.shuffle)}"
+  @@code = nil
+  @@board = nil
+
+  def self.setup
+    @@code = TestSetup.generate_code
+    @@board = Board.new(@@code)
+  end
+
+  def self.test_board_setup
+    p "board: #{@@board}"
+    p "check code guess: #{@@board.correct?(@@code)}"
+    p "check random guess: #{@@board.correct?(@@code.shuffle)}"
+  end
+  
+  def self.test_guess_feedback
+    p "feedback: #{@@board.feedback(@@code)}"
+    p "feedback: #{@@board.feedback(@@code.shuffle)}"
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
-  TestPlay.create_game
+  TestPlay.setup
+  TestPlay.test_board_setup
+  TestPlay.test_guess_feedback
 end
