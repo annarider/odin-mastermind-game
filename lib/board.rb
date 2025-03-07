011fbaaa806faa4f48bed12e6dedd9bdca8d6683
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-07 09:49:17 -0700

    Add: Init the board class with instance variables
    Creating the minimal instance of the Board class
    to be able to complete a full game. Initialize the
    variables that Board is responsible for.

---
+++ lib/board.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Board represents the Mastermind game board.
+# It owns the game rules and mechanics.
+# For example, it validates guesses, tracks
+# the guess and hints histories, and
+# checks for game over conditions.
+#
+# @example Creating a new board
+#   board = Board.new
+class Board
+  attr_accessor :guess_history, :hint_history, :current_guess
+
+  def initialize(guess)
+    @guess_history = []
+    @hint_history = []
+    @current_guess = guess
+  end
+end
