1db1e46e9cb03c45f910f38df7d841674a6ae0e6
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-07 09:43:00 -0700

    Add: Test for checking SecretCode class works
    The test script generates and prints out a new
    code to check it's working correctly. I see the
    code gets randomized each time I call it.

--- test/test_setup.rb
+++ test/test_setup.rb
@@ -1,5 +1,9 @@
 require_relative '../lib/configuration'
+require_relative '../lib/secret_code'
 
 p Configuration::CODE_LENGTH
 p Configuration::CODE_VALUES
 p Configuration::NUMBER_OF_ROUNDS
+
+code = SecretCode.new
+p code
