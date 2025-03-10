d58f65b63e570fa862891f4628391921a9d2572d
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-10 11:28:44 -0600

    Update: The correct number of pegs on the board
    6 is too many pegs for most mastermind games. The
    games usually have 4 pegs in the secret code.

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -2,7 +2,7 @@
 
 # Configuration stores game settings.
 class Configuration
-  CODE_LENGTH = 6.freeze
+  CODE_LENGTH = 4.freeze
   CODE_VALUES = %w[R Y O G B P].freeze
   NUMBER_OF_ROUNDS = 12.freeze
 end
