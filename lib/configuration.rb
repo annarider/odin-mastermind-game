32b6cf076960a8129f9e1a738fa67a3789bf0303
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-13 11:59:14 -0600

    Update: Orange to Cyan because of gem colorize
    The gem colorize which I plan to use for displaying
    the color in the interface doesn't support orange.
    It supports cyan, so I'm switching the valid
    secret code color orange to cyan.

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -3,6 +3,6 @@
 # Configuration stores game settings.
 class Configuration
   CODE_LENGTH = 4.freeze
-  CODE_VALUES = %w[R Y O G B P].freeze
+  CODE_VALUES = %w[R Y G C B P].freeze
   NUMBER_OF_ROUNDS = 12.freeze
 end
