32b5f378c2fddf8237368b9d7f00ecc61d813fcd
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-18 11:14:19 -0600

    Update: Rubocop autocorrect style issues
    Rubocop warned about the use of class variables
    (@@code, @@board, @@guess). Per Claude's advice,
    I changed to use Class instance variables
    (@code, @board, @guess) which belong to the class
    itself to avoid subtle bugs that might result
    from inheritance.

--- test/play.rb
+++ test/play.rb
@@ -8,38 +8,38 @@ require_relative '../lib/board'
 require_relative '../lib/interface'
 
 module TestPlay
-  @@code = nil
-  @@board = nil
-  @@guess = nil
+  class << self
+    attr_accessor :code, :board, :guess
+  end
 
   def self.setup
-    @@code = TestSetup.generate_code
-    @@guess = TestSetup.generate_code
-    @@board = Board.new(@@code, @@guess)
-    p "code: #{@@code}, guess: #{@@guess}"
+    @code = TestSetup.generate_code
+    @guess = TestSetup.generate_code
+    @board = Board.new(@code, @guess)
+    p "code: #{@code}, guess: #{@guess}"
   end
 
   def self.test_board_setup
-    p "board: #{@@board}"
-    p "check code guess: #{@@board.win?(@@code)}"
-    p "check random guess: #{@@board.win?(@@guess)}"
+    p "board: #{@board}"
+    p "check code guess: #{@board.win?(@code)}"
+    p "check random guess: #{@board.win?(@guess)}"
   end
 
   def self.test_guess_feedback
-    p "feedback: #{@@board.feedback(@@code)}"
-    p "feedback: #{@@board.feedback(@@guess)}"
+    p "feedback: #{@board.feedback(@code)}"
+    p "feedback: #{@board.feedback(@guess)}"
   end
 
   def self.test_gameover
-    @@board.current_guess = @@code ## have Game class call to set guess
-    p "gameover with code? #{@@board.game_over?}, #{@@code}"
-    @@board.current_guess = @@guess
-    p "gameover with guess? #{@@board.game_over?}, #{@@guess}}"
+    @board.current_guess = @code ## have Game class call to set guess
+    p "gameover with code? #{@board.game_over?}, #{@code}"
+    @board.current_guess = @guess
+    p "gameover with guess? #{@board.game_over?}, #{@guess}}"
   end
 
   def self.test_board_history
-    p "history: #{@@board.history}"
-    p "hint: #{@@board.hint}"
+    p "history: #{@board.history}"
+    p "hint: #{@board.hint}"
   end
 
   def self.test_interface
@@ -48,7 +48,7 @@ module TestPlay
     guess = interface.request_guess
     p guess
     p interface.valid?(guess)
-    interface.show_board(@@board)
+    interface.show_board(@board)
   end
 end
 
