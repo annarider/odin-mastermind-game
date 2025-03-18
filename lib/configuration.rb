0da003ead9fa1bb5b19db222a318c19363a151cb
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-18 10:07:07 -0600

    Update: Rubocop autocorrect style issues

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -2,7 +2,7 @@
 
 # Configuration stores game settings.
 class Configuration
-  CODE_LENGTH = 4.freeze
+  CODE_LENGTH = 4
   CODE_VALUES = %w[R Y G C B P].freeze
-  NUMBER_OF_ROUNDS = 12.freeze
+  NUMBER_OF_ROUNDS = 12
 end
