97d6c6e5a06596abc23fee40d018a200601040db
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-20 11:09:03 -0600

    Add: New Computer class to handle computer player
    Initialize this Computer class which manages the
    logic for the computer player.

---
+++ lib/computer.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Computer class controls the computer
+# as the guesser player in the Mastermind game.
+# It handles computer's guesses, analyzing
+# hints to pick the next best guess.
+#
+# @example Creating a new computer player
+#   computer = Computer.new
+class Computer
+  require_relative 'configuration'
+  
+  def initialize
+  end
+
+  def first_guess
+    p Configuration::CODE_VALUES * Configuration::CODE_LENGTH
+  end
+end
