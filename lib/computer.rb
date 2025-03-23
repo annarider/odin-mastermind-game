40544bf38d2eaef72eeb10a7649eaf9aa5435592
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-23 15:30:32 -0600

    Add: Method to analyze_feedback to use hints
    The method will take hints and consider the next
    guess. It required adding new instance variables
    to track the code length. Since code length was
    a constant and frozen, using .dup allowed me to
    make an unfrozen copy.

--- lib/computer.rb
+++ lib/computer.rb
@@ -9,21 +9,36 @@
 #   computer = Computer.new
 class Computer
   require_relative 'configuration'
-  attr_accessor :all_codes, :colors, :last_guess
+  attr_accessor :code_length, :all_codes, :possible_codes, :last_guess
   
   def initialize
-    @colors = Configuration::CODE_VALUES
-    @all_codes = @colors.repeated_permutation(Configuration::CODE_LENGTH).to_a
-    @last_guess = (colors[0] + colors[0] + colors[1] + colors[1]).chars
+    @code_length = Configuration::CODE_LENGTH
+    @possible_codes = Configuration::CODE_VALUES.dup
+    @all_codes = @possible_codes.repeated_permutation(code_length).to_a
+    @last_guess = first_guess
   end
 
   def guess(guess_history, hint_history)
     return last_guess if hint_history.empty?
 
-    guess = colors[0] * Configuration::CODE_LENGTH
-    guess.chars
+    guess = analyze_feedback(hint_history)
+    guess
   end
 
   private
+
+  def first_guess
+    (possible_codes[0] * code_length).chars
+  end
+
+  def analyze_feedback(hints)
+    possible_codes.shift
+    exact_match = hints[-1][0]
+    return first_guess if exact_match == 0
+      
+    last_guess.pop(exact_match.to_i)
+    elements_to_replace = code_length - exact_match
+    elements_to_replace.times { last_guess.push(possible_codes[0]) }
+  end
 
 end
