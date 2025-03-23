b2dae16e76ce0f65e014c97aaf1bd00ce070aa07
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-23 16:05:50 -0600

    Update: analyze_feedback to handle different hints
    The analyze_feedback method should do different
    behavior if the hint shows non-0 exact matches
    and colored matches. Still more to do for
    completing this method.

--- lib/computer.rb
+++ lib/computer.rb
@@ -34,11 +34,14 @@ class Computer
   def analyze_feedback(hints)
     possible_codes.shift
     exact_match, color_match = hints[-1][0], hints[-1][1]
-    return first_guess if exact_match == 0 && color_match == 0
-      
-    elements_to_replace = code_length - exact_match
-    last_guess.pop(elements_to_replace)
-    elements_to_replace.times { last_guess.push(possible_codes[0]) }
+    if exact_match == 0 && color_match == 0
+      @last_guess = first_guess
+    elsif exact_match > 0 && color_match == 0
+      elements_to_replace = code_length - exact_match
+      last_guess.pop(elements_to_replace)
+      elements_to_replace.times { last_guess.push(possible_codes[0]) }
+    end
+    last_guess
   end
 
 end
