42db8beb285ed8dd9afa3bd01b22b0d46b21f6b4
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-24 11:19:13 -0600

    Add: Method to sort codes to make new guess
    When the feedback adds up to 4, all the colors
    are correct but they may be in incorrect positions.
    
    Instead of randomly guessing across all codes, I'd
    rather use some kind of random guessing within
    the correct colors. Ideally, I'd like to implement
    some kind of bubble sort to switch the order of
    the characters but I'd need to keep track of
    indices, so I haven't implemented that yet.

--- lib/computer.rb
+++ lib/computer.rb
@@ -23,7 +23,7 @@ class Computer
   def guess(guess_history, hint_history)
     return last_guess if hint_history.empty?
 
-    analyze_feedback(hint_history)
+    analyze_feedback(guess_history, hint_history)
   end
 
   private
@@ -32,31 +32,38 @@ class Computer
     (possible_codes[0] * code_length).chars
   end
 
-  def analyze_feedback(hints)
+  def analyze_feedback(guesses, hints)
     deleted_color = possible_codes.shift
     exact_match, color_match = hints[-1][0], hints[-1][1]
     if exact_match == 0 && color_match == 0
-      remove_all_color(deleted_color)
+      remove_codes(deleted_color)
       @last_guess = first_guess
     else
-      next_guess(exact_match, color_match) 
+      next_guess(guesses, exact_match, color_match) 
     end
     last_guess
   end
   
-  def next_guess(exact_match, color_match)
-    if (exact_match + color_match) == 4
-      random_pick
-    elements_to_replace = code_length - exact_match
-    last_guess.pop(elements_to_replace)
-    elements_to_replace.times { last_guess.push(possible_codes[0]) }
+  def next_guess(guesses, exact_match, color_match)
+    return sort(guesses, last_guess) if (exact_match + color_match) == 4
+    
+    if exact_match > 0
+      elements_to_replace = code_length - exact_match
+      last_guess.pop(elements_to_replace)
+      elements_to_replace.times { last_guess.push(possible_codes[0]) }
+    end
   end
-
+  
   def random_pick
-    all_codes.sample
+    all_codes.shuffle
   end
 
-  def remove_all_colors(color)
+  def remove_codes(color)
     all_codes.delete_if { |code| code.include?(color) }
+  end
+
+  def sort(guesses, code)
+    guess = code.shuffle until guesses.exclude?(guess)
+    guess
   end
 end
