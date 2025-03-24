2ef341588fcf3a81ab1129ce0f23e4cefeef79ff
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-24 14:04:54 -0600

    Update: Sort logic inspired by Bubble Sort
    Replace the randomly picking option with the logic
    borrowed from Bubble Sort. Deleted the
    bubble_sort.rb script since I added the logic into
    the Computer class.

--- lib/computer.rb
+++ lib/computer.rb
@@ -10,20 +10,21 @@
 class Computer
   require_relative 'configuration'
   attr_accessor :code_length, :all_codes, :possible_codes, 
-    :last_guess, :correct_codes
+    :last_guess, :correct_codes, :position
   
   def initialize
     @code_length = Configuration::CODE_LENGTH
     @possible_codes = Configuration::CODE_VALUES.dup
     @all_codes = @possible_codes.repeated_permutation(code_length).to_a
     @last_guess = first_guess
-    @correct_codes = nil
+    @correct_codes = []
+    @position = 0
   end
 
   def guess(guess_history, hint_history)
     return last_guess if hint_history.empty?
 
-    analyze_feedback(guess_history, hint_history)
+    analyze_feedback(hint_history)
   end
 
   private
@@ -32,38 +33,46 @@ class Computer
     (possible_codes[0] * code_length).chars
   end
 
-  def analyze_feedback(guesses, hints)
+  def analyze_feedback(hints)
     deleted_color = possible_codes.shift
     exact_match, color_match = hints[-1][0], hints[-1][1]
     if exact_match == 0 && color_match == 0
       remove_codes(deleted_color)
       @last_guess = first_guess
     else
-      next_guess(guesses, exact_match, color_match) 
+      @last_guess = next_guess(deleted_color, exact_match, color_match) 
     end
     last_guess
   end
   
-  def next_guess(guesses, exact_match, color_match)
-    return sort(guesses, last_guess) if (exact_match + color_match) == 4
+  def next_guess(color, exact_match, color_match)
+    number_feedback = exact_match + color_match
+    @correct_codes.push([color, number_feedback])
+    return sort if number_feedback == 4
     
-    if exact_match > 0
-      elements_to_replace = code_length - exact_match
-      last_guess.pop(elements_to_replace)
-      elements_to_replace.times { last_guess.push(possible_codes[0]) }
-    end
+    replace_code_elements(color, number_feedback)
   end
-  
+    
   def random_pick
     all_codes.shuffle
   end
-
+  
   def remove_codes(color)
     all_codes.delete_if { |code| code.include?(color) }
   end
+  
+  def sort
+    last_guess[position], last_guess[position + 1] = last_guess[position + 1], last_guess[position]
+    @position += 1
+    last_guess
+  end
+  
+  def replace_code_elements(color, number)
+    remove_codes(color)
 
-  def sort(guesses, code)
-    guess = code.shuffle until guesses.exclude?(guess)
-    guess
+    elements_to_replace = code_length - number
+    last_guess.pop(elements_to_replace)
+    elements_to_replace.times { last_guess.push(possible_codes[0]) }
+    last_guess
   end
 end
